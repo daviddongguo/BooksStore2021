9dde0cb397a5ababc0ffd382c43a7d01369b6622
daviddongguo <david.dong.guo@gmail.com>
2020-12-23 18:51:54 -0500

    Edit home to display product list

--- BooksStore2021.Mvc/Controllers/HomeController.cs
+++ BooksStore2021.Mvc/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using BooksStore2021.Mvc.Models;
+﻿using BooksStore2021.Classlib.Services;
+using BooksStore2021.Mvc.Models;
+using BooksStore2021.Mvc.Models.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System.Diagnostics;
@@ -8,15 +10,20 @@ namespace BooksStore2021.Mvc.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly EFDbContext _ctx;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, EFDbContext ctx)
         {
             _logger = logger;
+            _ctx = ctx;
         }
 
         public IActionResult Index()
         {
-            return View();
+            HomeViewModel homeViewModel = new HomeViewModel{
+                Products = _ctx.Products,
+            };
+            return View(homeViewModel);
         }
 
         public IActionResult Privacy()
