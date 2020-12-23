2bbbe7ea32828e1d013efef8b4014cacde2af385
daviddongguo <david.dong.guo@gmail.com>
2020-12-22 20:29:45 -0500

    Add other models

---
+++ BooksStore2021.Classlib/Entities/EmailSettings.cs
@@ -0,0 +1,15 @@
+﻿namespace BooksStore2021.Classlib.Entities
+{
+    public class EmailSettings
+    {
+        public string MailToAddress = "orders@example.com";
+        public string MailFromAddress = "BooksStore@example.com";
+        public bool UseSsl = true;
+        public string Username = "MySmtpUsername";
+        public string Password = "MySmtpPassword";
+        public string ServerName = "smtp.example.com";
+        public int ServerPort = 587;
+        public bool WriteAsFile = true;
+        public string FileLocation = @"c:\booksStore_emails";
+    }
+}
