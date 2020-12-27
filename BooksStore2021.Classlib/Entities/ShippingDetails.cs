5cf7e88bc47f5e23571405d31a4da0edfb275e10
daviddongguo <david.dong.guo@gmail.com>
2020-12-27 15:58:51 -0500

    Make shipping details simple

--- BooksStore2021.Classlib/Entities/ShippingDetails.cs
+++ BooksStore2021.Classlib/Entities/ShippingDetails.cs
@@ -5,20 +5,20 @@ namespace BooksStore2021.Classlib.Entities
 {
     public class ShippingDetails
     {
-        [Required(ErrorMessage = "Please enter a name")]
+        //[Required(ErrorMessage = "Please enter a name")]
         public string Name { get; set; }
-        public string Email { get; set; }
-        [Required(ErrorMessage = "Please enter the first address line")]
+        public string Email { get; set; } = "guest@email.com";
+        //[Required(ErrorMessage = "Please enter the first address line")]
         [Display(Name = "Main address")]
         public string Line1 { get; set; }
         public string Line2 { get; set; }
         public string Line3 { get; set; }
-        [Required(ErrorMessage = "Please enter a city name")]
+        //[Required(ErrorMessage = "Please enter a city name")]
         public string City { get; set; }
-        [Required(ErrorMessage = "Please enter a state or province name")]
+        //[Required(ErrorMessage = "Please enter a state or province name")]
         public string State { get; set; }
         public string Zip { get; set; }
-        [Required(ErrorMessage = "Please enter a country name")]
+        //[Required(ErrorMessage = "Please enter a country name")]
         public string Country { get; set; }
         public bool GiftWrap { get; set; }
 
