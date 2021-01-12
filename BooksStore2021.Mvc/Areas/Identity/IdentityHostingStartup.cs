a0349402ba7adba938ff0e9021a697b5f0b0cfa2
daviddongguo <david.dong.guo@gmail.com>
2021-01-11 23:33:04 -0500

    Apply Scaffolded Identity automatically

---
+++ BooksStore2021.Mvc/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using BooksStore2021.Classlib.Services;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(BooksStore2021.Mvc.Areas.Identity.IdentityHostingStartup))]
+namespace BooksStore2021.Mvc.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
