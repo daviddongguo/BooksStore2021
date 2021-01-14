eb216e977b81c3937fcd70fb14f802c206ec30e1
daviddongguo <david.dong.guo@gmail.com>
2021-01-14 00:00:42 -0500

    Add variables config file

---
+++ BooksStore2021.Mvc/EnvironmentalVariables.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BooksStore2021.Mvc
+{
+    public static class EnvironmentalVariables
+    {
+        public const string SessionCart = "ShoppingCartSession";
+
+        public const string AdminRole = "Admin";
+        public const string CustomerRole = "Customer";
+
+        public const string EmailAdmin = "admin@booksstore.com";
+
+        public const string AdminPassword = "#d%GT34M^g";
+    }
+}
