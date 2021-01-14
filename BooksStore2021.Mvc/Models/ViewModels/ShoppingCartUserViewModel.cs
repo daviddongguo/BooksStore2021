a3496b1104b28e96c508d55b1f0aaee8e5b0e9c1
daviddongguo <david.dong.guo@gmail.com>
2021-01-14 00:04:11 -0500

    Add shopping cart summary

---
+++ BooksStore2021.Mvc/Models/ViewModels/ShoppingCartUserViewModel.cs
@@ -0,0 +1,15 @@
+﻿using BooksStore2021.Classlib.Entities;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BooksStore2021.Mvc.Models.ViewModels
+{
+    public class ShoppingCartUserViewModel
+    {
+        public ShoppingCart ShoppingCart { get; set; } = new ShoppingCart();
+        public IdentityUser User { get; set; }
+    }
+}
