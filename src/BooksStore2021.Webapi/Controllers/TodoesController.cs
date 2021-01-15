216a6902fb0cd724be047e40e745a6672cefb48f
daviddongguo <david.dong.guo@gmail.com>
2021-01-15 12:00:56 -0500

    Add Todo controller

---
+++ src/BooksStore2021.Webapi/Controllers/TodoesController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace BooksStore2021.Webapi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TodoesController : ControllerBase
+    {
+        // GET: api/<TodoesController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<TodoesController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<TodoesController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<TodoesController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<TodoesController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
