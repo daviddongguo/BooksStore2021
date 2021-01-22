89bf6c07560d24a782114aa238a955e60da3c463
daviddongguo <david.dong.guo@gmail.com>
2021-01-22 11:32:33 -0500

    Add unitofwork for oder

---
+++ src/BooksStore2021.Classlib/Abstract/IOrderUnitOfWork.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace BooksStore2021.Classlib.Abstract
+{
+    public interface IOrderUnitOfWork : IDisposable
+    {
+        public IOrderRepository Orders { get; }
+        public IShoppingCartRepository ShoppingCarts { get; }
+        public IShippingDetailsRepository ShippingDetails { get; }
+        Task<int> Complete();
+    }
+}
---
+++ src/BooksStore2021.Classlib/Concrete/OrderUnitOfWork.cs
@@ -0,0 +1,33 @@
+﻿using BooksStore2021.Classlib.Abstract;
+using System;
+using System.Threading.Tasks;
+
+namespace BooksStore2021.Classlib.Concrete
+{
+    public class OrderUnitOfWork : IOrderUnitOfWork
+    {
+        private readonly EFDbContext _db;
+
+        public OrderUnitOfWork(EFDbContext db)
+        {
+            _db = db;
+            Orders = new EFOrderRepository(db);
+            ShoppingCarts = new EFShoppingCartRepository(db);
+            ShippingDetails = new EFShippingDetailsRepository(db);
+        }
+
+        public IOrderRepository Orders { get; private set; }
+        public IShoppingCartRepository ShoppingCarts { get; private set; }
+        public IShippingDetailsRepository ShippingDetails { get; private set; }
+
+        public async Task<int> Complete()
+        {
+            return await _db.SaveChangesAsync();
+        }
+
+        public void Dispose()
+        {
+            _db.Dispose();
+        }
+    }
+}
